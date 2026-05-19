0246745ffe0076d490bdb5010a43f51afe514d81
Kate-art444 <feumatiokate@gmail.com>
2026-05-19 12:25:03 +0100

    making other buttons functional

--- JS/script.js
+++ JS/script.js
@@ -8,47 +8,67 @@ allButtons.forEach(singleButton => {
         const buttonText = singleButton.innerText;
         const valueToAdd = buttonText;
 
-        if (valueToAdd === "x^y") {
+        if (valueToAdd === "^") {
             valueToAdd = "**";
+             return;
 
         }
         else if (valueToAdd === "EXP") {
              valueToAdd = "*10**";
+              return;
         }
 
-        output.value += valueToAdd;
+       
            
 
         if (buttonText === "AC") {
             output.value = "";
+             return;
         }
 
         else if (buttonText === "DEL") {
             output.value = output.value.slice(0, -1);
+             return;
         }
 
-         else if (buttonText === "=") {
-            output.value = eval(output.value);
-        }
+       
 
-        else if(buttonText === "=") {
-            try {
-                 output.value = eval(output.value);
-            }
-    
-        catch {
-        output.value = "error";
-        }
-
-    }
+        
 
     else if (buttonText === "OFF") {
         output.value = "";
         output.style.backgroundColor = "black";
+         return;
+    }
+   
+
+    else if(buttonText === "ON"){
+        output.style.backgroundColor = "rgb(57, 164, 231)"
+         return;
+    }
+
+    else if (buttonText === "=") {
+
+        try {
+            let currentValue = output.value;
+
+            currentValue = currentValue.replace(/√(\d+\.?\d*)/g, "Math.sqrt($1)");
+
+             currentValue = currentValue.replace(/(\d+\.?\d*)√/g, "Math.sqrt($1)");
+
+            output.value = eval(currentValue);
+        }
+        
+       catch {
+        output.value = "Error";
+       }
     }
 
     else {
         output.value += buttonText;
+         return;
     }
     });
 });
+
+
